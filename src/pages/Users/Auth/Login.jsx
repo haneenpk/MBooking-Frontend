0455ecf09ab603667bedf22271637b7e9acfbe08
haneenpk <mohdhaneenpk666@gmail.com>
2024-05-09 20:13:49 +0530

    checking working env url third time

--- src/pages/Users/Auth/Login.jsx
+++ src/pages/Users/Auth/Login.jsx
@@ -35,7 +35,7 @@ const Login = ({ role }) => {
             await loginSchema.validate(formData, { abortEarly: false });
             setErrors({});
 
-            console.log("back: ",import.meta.env.VITE_AXIOS_BASE_URL);
+            console.log("back:",import.meta.env.VITE_AXIOS_BASE_URL);
 
             const response = await axios.post(`${import.meta.env.VITE_AXIOS_BASE_URL}/api/${role}/login`, formData);
 
